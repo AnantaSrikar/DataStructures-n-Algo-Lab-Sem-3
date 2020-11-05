25fc0d0f677eb5b4590ab4573430e11a5698aa87
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2020-11-05 19:27:40 +0530

    Solved second problem

--- assignment_4/p1.c
+++ assignment_4/p1.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 	for(int i = 1; i <= argc - 1; i++)
 	{
 		for(int j = 0; j < strlen(argv[i]); j++)
-			if(!isdigit(argv[i][j]))
+			if(!isdigit(argv[i][j]) && argv[i][j] != '-')
 			{
 				printf("error"); // Please enter only numbers!
 				return -1;
@@ -100,7 +100,7 @@ void exer_1(int input_arr[], int size)
 {
 	if(size < 2)
 	{
-		printf("error"); // Too less arguments!
+		printf("error"); // No elements in the linked list!
 		exit(0);
 	}
 
@@ -125,9 +125,78 @@ void exer_1(int input_arr[], int size)
 		printf("1");
 }
 
+
+int isPrime(int num)
+{
+	if(num <= 1)
+		return 0;
+
+	else if(num == 2)
+		return 1;
+		
+	for(int i = num - 1; i >= 2; i--)
+		if(num % i == 0)
+			return 0;
+	
+	return 1;
+}
+
 void exer_2(int input_arr[], int size)
 {
-	// TODO: finish it
+	if(size < 2)
+	{
+		printf("error"); // No elements in the linked list!
+		exit(0);
+	}
+
+	node *first_node_ptr, *temp, *prime_nos = NULL, *prime_temp;
+
+	first_node_ptr = get_linked_list(input_arr, size);
+	
+	temp = first_node_ptr;
+
+	
+
+	while(temp != NULL)
+	{
+		if(isPrime(temp -> data))
+		{
+			if(prime_nos == NULL)
+			{
+				prime_nos = (node*)malloc(sizeof(node));
+				prime_temp = prime_nos;
+
+				prime_temp -> data = temp -> data;
+			}
+
+			else
+			{
+				node *new_node = (node*)malloc(sizeof(node));
+
+				new_node -> data = temp -> data;
+
+				prime_temp -> next = new_node;
+				prime_temp = new_node;
+			}
+		}
+		temp = temp -> next;
+	}
+
+	if(prime_nos != NULL)
+		prime_temp -> next = NULL;
+
+	temp = prime_nos;
+
+	while(temp != NULL)
+	{
+		if(temp -> next != NULL)
+			printf("%d ", temp -> data);
+		else
+			printf("%d", temp -> data);
+		
+		temp = temp -> next;
+	}
+
 }
 
 void exer_3(int input_arr[], int size)
